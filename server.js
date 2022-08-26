b2a9ed4e771ff9950f60c3b9dcc62cedd1483ea6
Ayush Sinha <ayushsinhajsr20@gmail.com>
2022-08-26 13:27:32 +0530

    Changed connection style

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors');
 
+// Middlewares
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors());
 
-const port = "https://git.heroku.com/secure-reaches-31003.git"
+const port = process.env.PORT
 
 // Database connection
 const conn = require('./database_connection');
@@ -13,6 +16,16 @@ const conn = require('./database_connection');
 const routes = require('./routes/route')
 app.use(routes)
 
-app.listen(port,()=>{
-    console.log(`Server running at PORT ${port}`)
-})
+con.then(db => {
+    if(!db) return process.exit(1);
+
+    // listen to the http server 
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`)
+    })
+
+    app.on('error', err => console.log(`Failed To Connect with HTTP Server : ${err}`));
+    // error in mondb connection
+}).catch(error => {
+    console.log(`Connection Failed...! ${error}`);
+});
