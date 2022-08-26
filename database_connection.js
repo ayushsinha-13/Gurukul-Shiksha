f0862e4588331a2cc720b0eec716daebfa19984a
Ayush Sinha <ayushsinhajsr20@gmail.com>
2022-08-26 14:12:38 +0530

    Database file updated

--- database_connection.js
+++ database_connection.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config({path: '../.env'})
 
+console.log(process.env.MONGO_PORT)
+
 const conn = mongoose.connect(process.env.MONGO_PORT)
-    .then(db =>{
-        console.log("Database Connected")
-        return db
-    }).catch(err => {
-        console.log("Connection Error: " + err)
-})
+//     .then(db =>{
+//         console.log("Database Connected")
+//         return db
+//     }).catch(err => {
+//         console.log("Connection Error: " + err)
+// })
 
 module.exports = conn;
