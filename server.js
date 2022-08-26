b4c5ef1f1720bfb20ffefebfa76b1cbba926a32d
Ayush Sinha <ayushsinhajsr20@gmail.com>
2022-08-26 12:50:19 +0530

    Port changed to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+const port = process.env.PORT || 5000
 
 // Database connection
 const conn = require('./database_connection');
@@ -10,6 +13,6 @@ const conn = require('./database_connection');
 const routes = require('./routes/route')
 app.use(routes)
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log(`Server running at PORT 3000`)
 })
